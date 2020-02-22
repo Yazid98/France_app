eb74b1e4896b23bdab80dd1cf4e9c8eac44c1982
Yazid98 <ericbadarou@gmail.com>
2020-02-22 09:12:52 +0100

    The registration and the the save of informations in Database It doesn't work, but i will correct it later

--- app/src/main/java/com/example/france_app/RegistrationActivity.kt
+++ app/src/main/java/com/example/france_app/RegistrationActivity.kt
@@ -7,93 +7,79 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 
-@Suppress("UNUSED_VARIABLE", "SpellCheckingInspection")
-class RegistrationActivity : AppCompatActivity() {
+@Suppress("UNUSED_VARIABLE", "SpellCheckingInspection", "ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE",
+    "UNUSED_PARAMETER"
+)
+class RegistrationActivity(var users: Users) : AppCompatActivity() {
 
-    //lateinit variables are instances of Firebase which permit to SignUp
-     lateinit var auth: FirebaseAuth
-     lateinit var emailEt: EditText
-     lateinit var passwordEt: EditText
-     lateinit var signUpButton: Button
-
-     lateinit var firstname : String
-     lateinit var lastname : String
-     lateinit var age : String
-
-
+    var firstname = findViewById<EditText>(R.id.signUp_prenom)
+    var lastname = findViewById<EditText>(R.id.signUp_nom)
+    var age = findViewById<EditText>(R.id.signUp_Age)
+    var email = findViewById<EditText>(R.id.signUp_email)
+    var password = findViewById<EditText>(R.id.SignUp_password)
+    var signUpBtn = findViewById<Button>(R.id.validateRegistration)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_)
 
-        // To display the image
-        val mImageView = findViewById<ImageView>(R.id.logo1)
-        mImageView.setImageResource(R.drawable.image_logo1)
+
+        //For Firebase
+        var database: FirebaseDatabase
+        var mdatabase: DatabaseReference
 
 
-        auth = FirebaseAuth.getInstance()
+        signUpBtn.setOnClickListener {
 
-        //Registration
-        emailEt = findViewById(R.id.signUp_email)
-        passwordEt = findViewById(R.id.SignUp_password)
+            val email: String = email.text.toString()
+            val password: String = password.text.toString()
 
-        firstname = findViewById<EditText>(R.id.signUp_prenom).toString()
-        lastname = findViewById<EditText>(R.id.signUp_nom).toString()
-        age = findViewById<EditText>(R.id.signUp_nom).toString()
-        signUpButton = findViewById(R.id.validateRegistration)
+            val firstname: String = firstname.text.toString()
+            val lastname: String = lastname.text.toString()
+            val age: String = age.text.toString()
 
-
-        var email: String = emailEt.text.toString()
-        var password: String = passwordEt.text.toString()
-
-
-        //Here we have to cast variables to text format so Firebase will save them.
-
-
-            signUpButton.setOnClickListener {
-                //If there are no input
-                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-                    Toast.makeText(this, "Remplissez tout les champs SVP", Toast.LENGTH_LONG).show()
-                } else {
-
-                    //This is a function of firebase to SignUp with email and password
-                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this,
-                        OnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                Toast.makeText(this, "Vous êtes bien enregistré", Toast.LENGTH_LONG).show()
-                                //savehero()
-                                val newActivity1 = Intent(this, Home_Activity::class.java)
-                                startActivity(newActivity1)
-                                finish()
-                            } else {
-                                Toast.makeText(this, "Enregistrement non validé", Toast.LENGTH_LONG).show()
-                            }
-                        })
-
-                }
+            if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                var users = Users(email, password, firstname, lastname, age)
+                Toast.makeText(this, "Please fill all the gaps", Toast.LENGTH_LONG).show()
             }
+            registerUser(email, password)
+        }
     }
 
-    private fun savehero() {
-        val firstname = firstname
-        val lastname = lastname
-        val age = age
 
-        if ( TextUtils.isEmpty(firstname) || TextUtils.isEmpty(lastname) || TextUtils.isEmpty(age) ){
-            Toast.makeText(this, "Remplissez tous les champs svp", Toast.LENGTH_LONG).show()
+    private fun registerUser(email: String, password: String) {
+
+        val mAuth: FirebaseAuth
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this,
+            OnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Vous êtes bien enregistré", Toast.LENGTH_LONG).show()
+                    updateUI(users)
+                }
+            })
+    }
+
+    @Suppress("JoinDeclarationAndAssignment")
+    fun updateUI(currentUser: Users) {
+        val mdatabase: DatabaseReference
+        mdatabase = FirebaseDatabase.getInstance().reference
+        val keyId: String? = mdatabase.push().key
+        mdatabase.child(keyId.toString()).setValue(users).addOnCompleteListener {
+            Toast.makeText(this, "Vous êtes bien enregistré", Toast.LENGTH_LONG).show()
         }
-            val ref = FirebaseDatabase.getInstance().getReference("users")
-            val heroId = ref.push().key
-            val users = Users(heroId, firstname, lastname, age)
-            ref.child(heroId.toString()).setValue(users).addOnCompleteListener {
-                Toast.makeText(this, "Utilisateur bien enregistrer", Toast.LENGTH_LONG).show()
-            }
+        val intent = Intent(this@RegistrationActivity, Home_Activity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        private const val USERS = "users"
     }
 }
--- app/src/main/java/com/example/france_app/Users.kt
+++ app/src/main/java/com/example/france_app/Users.kt
@@ -5,39 +5,46 @@ package com.example.france_app
 
 @Suppress("ConvertSecondaryConstructorToPrimary", "RedundantGetter", "RedundantSetter", "unused")
 class Users {
-    constructor(
-        id: String?,
-        firstname: String,
-        lastname: String,
-        age: String
-    ) {
-        this.id = id.toString()
+
+
+
+    var email : String =""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var password : String =""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var firstname : String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var lastname : String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var age : String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    constructor(email: String, password: String, firstname: String, lastname: String, age: String) {
+        this.email = email
+        this.password = password
         this.firstname = firstname
         this.lastname = lastname
         this.age = age
     }
 
-    private var id : String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
 
-    private var firstname: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var lastname: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var age: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
 }
